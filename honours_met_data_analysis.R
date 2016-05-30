ba5776c0378654809803f6d183cb3d899373cc89
KaitlynLieschke <kl729@uowmail.edu.au>
2016-05-30 17:13:57 +1000

    Import more stations

--- honours_met_data_analysis.R
+++ honours_met_data_analysis.R
@@ -1,20 +1,25 @@
 # Import data
-bowral_min<-import(file = file.choose(), file.type = "csv",sep = ",", header.at = 1, data.at = 2, date = "Date",
+campbelltown_max<-import(file = file.choose(), file.type = "csv",sep = ",", header.at = 1, data.at = 2, date = "Date",
                     date.format = "%Y%m%d", na.strings = c("", "NAN","-9999", "NA" ),correct.time = NULL)
 
 # Identify site for plotting and set variables
+# Uncomment relevant site information
 dataset=albionpark_min
 sitemet="Albion_Park_(Wollongong_Airport)_Temperature_Minimum"
 plottitle="Trend in Minimum Temperature at Albion Park"
-
+'''
 dataset=bellambi_min
 sitemet="Bellambi_Aws_Temperature_Minimum"
 plottitle="Trend in Minimum Temperature at Bellambi"
 
 dataset=bowral_min
-sitemet="Bowral_(Parry_drive)_Temperature_Minimum"
+sitemet="Bowral_(Parry_Drive)_Temperature_Minimum"
 plottitle="Trend in Minimum Temperature at Bowral"
 
+dataset=campbelltown_min
+sitemet="Campbelltown_(Mount_Annan)_Temperature_Minimum"
+plottitle="Trend in Minimum Temperature at Campbelltown"
+'''
 # Time plot
 timePlot(albionpark_max, pollutant=sitemet,plot.type="p",main="Maximum Temperature at Albion Park",
          ylab=expression(Temperature ~ (C)),pch=20,cex=0.25)
